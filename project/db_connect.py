d0ee33eb4df86600e40a47e97a448a184dea64bf
Abdur Rehman <abdur.rehman8872@gmail.com>
2026-07-06 16:54:31 +0500

    task practice project crud done with postgres integrated

--- project/db_connect.py
+++ project/db_connect.py
@@ -1,7 +1,6 @@
-from sqlmodel import SQLModel, Field, create_engine, Session
-from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Annotated
-import uvicorn
 import os
 from dotenv import load_dotenv
 
@@ -22,11 +21,18 @@ class TaskCreate(TaskBase):
 class Task(TaskBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
 
+
 class TaskResponse(Task):
     pass
 
 
-DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/postgres"
+class TaskUpdate(SQLModel):
+    title: str | None = None
+    done: bool | None = None
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
 
 print("Creating tables...")
@@ -41,7 +47,8 @@ def get_session():
 SessionDep = Annotated[Session, Depends(get_session)]
 
 
-@app.post("/task", response_model=TaskResponse)
+# create
+@app.post("/task", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
 def create_task(new_task: TaskCreate, session: SessionDep) -> TaskResponse:
     db_task = Task.model_validate(new_task)
     session.add(db_task)
@@ -51,6 +58,43 @@ def create_task(new_task: TaskCreate, session: SessionDep) -> TaskResponse:
     return db_task
 
 
-# if __name__ == "__main__":
-#     # Start the server properly using uvicorn
-#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+# update
+@app.patch("/update-task/{task_id}", response_model=TaskResponse)
+def update_task(
+    task_id: int, updated_task: TaskUpdate, session: SessionDep
+) -> TaskResponse:
+    db_task = session.get(Task, task_id)
+    if not db_task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
+
+    updated = updated_task.model_dump(exclude_unset=True)
+
+    for k, v in updated.items():
+        setattr(db_task, k, v)
+
+    session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+
+    return db_task
+
+# read
+@app.get("/tasks", response_model=list[TaskResponse])
+def read_tasks(session: SessionDep):
+    statement = select(Task)
+    tasks = session.exec(statement).all()
+
+    return tasks
+
+# delete
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_200_OK)
+def delete_task(task_id:int, session:SessionDep):
+    db_task = session.get(Task, task_id)
+
+    if not db_task:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
+
+    session.delete(db_task)
+    session.commit()
+
+    return {"message":f"task with ID:{db_task.id} deleted successfully"}
